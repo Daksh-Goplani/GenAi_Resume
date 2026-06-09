c7910d9ae9c64bbc591d04121b54bf662277ec82
Daksh Goplani <dakshgoplani7@gmail.com>
2026-06-09 15:35:14 +0530

    Created Interview report model

---
+++ Backend/src/models/interviewReport.model.js
@@ -0,0 +1,91 @@
+const mongoose = require('mongoose')
+
+const technicalQuestionSchema = new mongoose.Schema({
+    question: {
+        type: String,
+        required: [true, 'Technical question is required']
+    },
+    intention: {
+        type: String,
+        require: [true, 'Intention is required']
+    },
+    answer: {
+        type: String,
+        require: [true, 'Answer is required']
+    }
+}, {
+    _id: false
+})
+
+const behaviourQuestionSchema = new mongoose.Schema({
+    question: {
+        type: String,
+        required: [true, 'Technical question is required']
+    },
+    intention: {
+        type: String,
+        require: [true, 'Intention is required']
+    },
+    answer: {
+        type: String,
+        require: [true, 'Answer is required']
+    }
+}, {
+    _id: false
+})
+
+const skillGapSchema = new mongoose.Schema({
+    skill: {
+        type: String,
+        required: [true, 'Skill is required']
+    },
+    severiry: {
+        type: String,
+        enum: ["low", "medium", "high"]
+    }
+}, {
+    _id: false
+})
+
+const preperationPlanSchema = new mongoose.Schema({
+    day: {
+        type: Number,
+        required: [true, "Day is required"]
+    },
+    focus: {
+        type: String,
+        required: [true, "Focus is required"]
+    },
+    tasks: [{
+        type: String,
+        required: [true, "Tasks are required"]
+    }]
+})
+
+const interviewReportSchema = new mongoose.Schema({
+    jobDescription: {
+        type: String,
+        required: [true, "Job description is required"]
+    },
+    resume: {
+        type: String
+    },
+    selfDesctiption: {
+        type: String
+    },
+    matchScore: {
+        type: Number,
+        min: 0,
+        max: 100
+    },
+    technicalQuestions: [technicalQuestionSchema],
+    behaviourQuestion: [behaviourQuestionSchema],
+    skillGap: [skillGapSchema],
+    preperationPlan: [preperationPlanSchema]
+}, {
+    timestamps: true
+})
+
+const interviewReportModel = mongoose.model("InterviewReport", interviewReportSchema)
+
+module.exports = interviewReportModel
