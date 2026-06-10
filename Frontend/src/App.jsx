4bb32ae252d7db2e8b2a4b045c29501a6436ef7d
Daksh Goplani <dakshgoplani7@gmail.com>
2026-06-10 15:35:57 +0530

    feat: implement Interview context and hooks for managing interview reports, enhance Home and Interview pages with report generation and navigation features

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { RouterProvider } from 'react-router'
 import { router } from './app.routes.jsx'
 import {AuthProvider} from './features/auth/Auth.context.jsx'
+import { InterviewProvider } from './features/interview/Interview.context.jsx'
+
 function App() {
 
   return (
     <AuthProvider>
+      <InterviewProvider>
       <RouterProvider router={router} />
+      </InterviewProvider>
     </AuthProvider>
   )
 }
